b8b3ee4ce5da0b5db8e75205238bfbffbd05c78a
JungsikNoh <jungsik.noh@utsouthwestern.edu>
2021-06-28 08:27:12 -0500

    06/27 update

--- COVID19_LatentActualInfectiousPopulation_Vaccination_parallel_State.R
+++ COVID19_LatentActualInfectiousPopulation_Vaccination_parallel_State.R
@@ -14,7 +14,7 @@
 options(bitmapType = 'cairo')
 
 #curDate = Sys.Date(); print(curDate)
-curDate = '2021-06-25'
+curDate = '2021-06-27'
 print(curDate)
 
 #setwd( )
--- doc/Daily_rates_TX_county.md
+++ doc/Daily_rates_TX_county.md
@@ -4,7 +4,7 @@
 
  <p>&nbsp;</p> 
 
-## 2021-06-25, COVID-19 Time Series
+## 2021-06-27, COVID-19 Time Series
 # TX counties (top 15)  
 
 
